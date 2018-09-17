25b024ce5428bdd5097f378647ee145dee280e6d
liamgak <gyuhyeon1999@gmail.com>
2018-09-18 03:00:24 +0900

    last_

--- main.py
+++ main.py
@@ -6,16 +6,9 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/HCI')
-def HCI():
-    return render_template('HCI.html')
-
-@app.route('/download/')
-def return_files_tut():
-	try:
-		return send_file('/home/site/wwwroot/download/[HCI-under]hw1_2017320229.hwp')
-	except Exception as e:
-		return str(e)
+@app.route('/hci')
+def hci():
+    return render_template('hci.html')
 
 if __name__ == '__main__':
     app.run()
